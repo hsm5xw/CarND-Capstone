5d58dfc661d1551c9c6878ad344667c4f4275e5b
hsm5xw <hsm5xw@virginia.edu>
2018-06-01 22:24:08 -0400

    change brake torque from 400 to 700 according to updated Udacity Note

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -106,7 +106,7 @@ class Controller(object):
         # If target linear velocity = 0, then go very slow        
         if linear_vel == 0. and current_vel < 0.1:
             throttle = 0.
-            brake    = 400   # N*m - to hold the car in place if we are stopped at a light. Acceleration - 1m/s^2
+            brake    = 700   # N*m - to hold the car in place if we are stopped at a light. Acceleration - 1m/s^2
         
         # If throttle is really small and velocity error < 0 (i.e. we're going faster than we want to be)
         elif throttle < 0.1 and vel_error < 0.:
